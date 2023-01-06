bd93cab752b4c7b0b2d860361c9bb719492bcf69
Rob Selkowitz <121113624+RobSelkowitz@users.noreply.github.com>
2023-01-06 13:21:55 -0500

    add color selector radio

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 createGrid(120);
-
-
+let bkgChoice = "gray";
+const colorChoices = document.querySelectorAll('input[name="bkgcolor"]');
+for (let i = 0; i<colorChoices.length; i++){
+    colorChoices[i].addEventListener("change", () =>{
+    bkgChoice = colorChoices[i].value;
+});
+}
 function createGrid(side){
 
     const gridHolder = document.querySelector('#gridHolder');
@@ -9,7 +14,7 @@ function createGrid(side){
         for(j=0; j < side; j++){
      const div = document.createElement("div");
      div.addEventListener('mouseover', function(e) 
-        {e.target.style.background = 'grey';})
+        {e.target.style.background = bkgChoice;})
      gridHolder.appendChild(div);
     }
     }
