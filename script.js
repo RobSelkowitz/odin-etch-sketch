1ccd949a47870dac63828abc2e4d92793a1a1d5e
Rob Selkowitz <121113624+RobSelkowitz@users.noreply.github.com>
2023-01-07 10:22:47 -0500

    cleanly reset grid with button

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 createGrid(120);
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', () =>{
+    createGrid(50);
+
+});
+
 let bkgChoice = "gray";
 const colorChoices = document.querySelectorAll('input[name="bkgcolor"]');
 for (let i = 0; i<colorChoices.length; i++){
@@ -9,10 +15,18 @@ for (let i = 0; i<colorChoices.length; i++){
 function createGrid(side){
 
     const gridHolder = document.querySelector('#gridHolder');
+
+/* clear any existing grid then draw new grid*/
+
+    const oldGridToClear = document.querySelectorAll('#gridDiv');
+    for (let gridDiv = 0; gridDiv<oldGridToClear.length; gridDiv++){
+            oldGridToClear[gridDiv].remove();
+    }
     
     for (let i=0; i < side; i++){
         for(j=0; j < side; j++){
      const div = document.createElement("div");
+     div.setAttribute('id', 'gridDiv');
      div.addEventListener('mouseover', function(e) 
         {e.target.style.background = bkgChoice;})
      gridHolder.appendChild(div);
